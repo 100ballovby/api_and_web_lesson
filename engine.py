b26aa9406b0eb6450382e9540d820093fe7cd239
ARCHITECT <greatraksin@icloud.com>
2021-12-24 20:36:04 +0300

    Adding fetch hotoscope function

--- engine.py
+++ engine.py
@@ -1,15 +1,16 @@
 import requests as r
 
 
-def fetch_horoscope():  # <- функция
+def fetch_horoscope(sign, day='tomorrow'):  # <- функция
     url = "https://sameer-kumar-aztro-v1.p.rapidapi.com/"
-    querystring = {"sign": "aquarius", "day": "today"}
+    querystring = {"sign": sign, "day": day}
     headers = {
         'x-rapidapi-host': "sameer-kumar-aztro-v1.p.rapidapi.com",
         'x-rapidapi-key': "e2eb357b7amsh926cfc62b8ec75cp17a448jsn5b614312d78a"
     }
     response = r.post(url, headers=headers, params=querystring)
-    return 'horoscope'
+    jr = response.json()  # превращаю ответ сервера в JSON-объект
+    return jr['description']  # возвращаю сам гороскоп
 
 
 def translate_horoscope():  # <- функция
--- worker.py
+++ worker.py
@@ -1,4 +1,5 @@
 from engine import fetch_horoscope, translate_horoscope
 
-print(fetch_horoscope())
-print(translate_horoscope())
+print(fetch_horoscope('virgo', 'today'))
+print(fetch_horoscope('virgo'))
+
